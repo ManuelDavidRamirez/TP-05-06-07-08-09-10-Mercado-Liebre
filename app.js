146039d7f3b0e436940fea4b399f4ec6b7903062
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-04-17 19:34:38 -0300

    html de login y register terminado

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get("/", (req,res) => {
     return res.sendFile(path.join(__dirname, "views", "home.html"))
 })
 
+app.get("/register", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "register.html"))
+})
+
+app.get("/login", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "login.html"))
+})
+
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`)
 })
